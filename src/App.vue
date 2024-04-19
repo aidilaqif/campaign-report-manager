19da03dc80c8d94a19f4be659cf323758ac6dd8e
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-04-19 15:26:14 +0800

    sidebar created

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,25 @@
     <div class="sidebar">
       <Sidebar />
     </div>
-    <!-- <div class="content"> -->
-      <!-- <Navbar /> -->
-      <!-- <ViewDetails/> -->
-      <!-- <div>viewContentCreators</div> -->
-    <!-- </div> -->
+    <div class="content">
+      <Navbar />
+      <ViewDetails/>
+      <div>viewContentCreators</div>
+    </div>
   </div>
 </template>
 <script>
-// import Login from './components/Login.vue';
 import Sidebar from './components/Sidebar.vue';
 import Navbar from './components/Navbar.vue';
-import Login from './components/Login.vue';
+// import Login from './components/Login.vue';
 //import ViewDetails from './components/ViewDetails.vue';
 
 export default {
   components: {
     Sidebar,
     Navbar,
-    //ViewDetails,
-    Login
+    // ViewDetails,
+    // Login
   }
 }
 </script>
@@ -32,4 +31,13 @@ export default {
     display : flex;
     align-items : column;
   }
+  .sidebar {
+  flex: 1; /* Sidebar takes 1 part of the available space */
+}
+
+.content {
+  flex: 2; /* Content takes 2 parts of the available space */
+  display: flex;
+  flex-direction: column;
+}
 </style>
