b0b6e68651be661ccd7282a3c8d0c806b57c8ebc
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-04-19 12:51:41 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
+  <!-- <Login /> -->
   <div class="container">
     <div class="sidebar">
       <Sidebar />
     </div>
-    <div class="content">
+    <!-- <div class="content"> -->
       <!-- <Navbar /> -->
-      <ViewDetails/>
-    <div>viewContentCreators</div></div>
+      <!-- <ViewDetails/> -->
+      <!-- <div>viewContentCreators</div> -->
+    <!-- </div> -->
   </div>
 </template>
 <script>
+// import Login from './components/Login.vue';
 import Sidebar from './components/Sidebar.vue';
 import Navbar from './components/Navbar.vue';
+import Login from './components/Login.vue';
 //import ViewDetails from './components/ViewDetails.vue';
 
 export default {
   components: {
     Sidebar,
     Navbar,
-    //ViewDetails
-  }  
+    //ViewDetails,
+    Login
+  }
 }
 </script>
 <style scoped>
